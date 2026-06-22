cb8b5acc8f7d6a854269e7a596a8b994078d65e9
Satendra Kushwaha <kushwahasatendra31@gmail.com>
2026-06-22 14:44:17 +0530

    Add database schema

---
+++ queries/schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE sales (
+    order_id INT,
+    order_date DATE,
+    customer_name VARCHAR(100),
+    product VARCHAR(100),
+    category VARCHAR(50),
+    quantity INT,
+    sales_amount DECIMAL(10,2),
+    city VARCHAR(50)
+);
